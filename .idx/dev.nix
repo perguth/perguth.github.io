3e704198f33dca5d4c71b569aa44337644f34bfa
idrathernotgetmails <idrathernotgetmails@gmail.com>
2025-05-05 17:24:14 +0000

    Add NIX file

---
+++ .idx/dev.nix
@@ -0,0 +1,45 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nano
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+          # and show it in IDX's web preview panel
+          command = ["/google/idx/builtins/bin/npx" "http-server"];
+          manager = "web";
+          env = {
+            # Environment variables to set for your server
+            PORT = "$PORT";
+          };
+        };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+      };
+    };
+  };
+}
